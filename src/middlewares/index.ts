a3d8d59e98ca3bd1d67a120a0b893523d467d001
xahid71 <jahid.info97@gmail.com>
2023-11-27 15:01:29 +0600

    update : middleware done

---
+++ src/middlewares/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import {get, merge} from 'lodash';
+
+import { getUsersBySessionToken } from 'db/users';
+
+
+export const isAuthenticate = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+
+    try {
+        const sessionToken = req.cookies['COOKIE-AUTHw'];
+        if(!sessionToken) {
+            return res.sendStatus(403);
+        } 
+        const exisitingUser =  await getUsersBySessionToken(sessionToken); 
+        if(!exisitingUser) {
+            return res.sendStatus(403 );
+        }
+        merge(req, {identity : exisitingUser});
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+
+
+}
